d45ce7345479c3ebaa9481b363bc9945dd1f6fb2
linderhp0607 <linderhidalgopinedo@gmail.com>
2025-01-11 18:32:50 -0500

    commit 2

---
+++ codigo/proyectoWebPoo2/src/main/java/com/app/proyecto/ProyectoWebPoo2Application.java
@@ -0,0 +1,13 @@
+package com.app.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoWebPoo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoWebPoo2Application.class, args);
+	}
+
+}
