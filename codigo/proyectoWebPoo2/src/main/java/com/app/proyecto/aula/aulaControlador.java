8b1d209b1e0e556835a82636ce830a9429486fcf
linderhp0607 <linderhidalgopinedo@gmail.com>
2025-01-13 19:50:19 -0500

    commit 4

--- codigo/proyectoWebPoo2/src/main/java/com/app/proyecto/aula/aulaControlador.java
+++ codigo/proyectoWebPoo2/src/main/java/com/app/proyecto/aula/aulaControlador.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class aulaControlador {
@@ -18,5 +19,17 @@ public class aulaControlador {
 		List<aula> listaAulas= repositorio.findAll();
 		modelo.addAttribute("listaAulas", listaAulas);
 		return "aulas";
+	}
+	
+	@GetMapping("/aulas/nuevo")
+	public String formularioNuevaAula(Model modelo) {
+		modelo.addAttribute("aula",new aula());
+		return "aula_formulario";
+	}
+	
+	@PostMapping("/aulas/guardar")
+	public String guardarAula(aula aula) {
+		repositorio.save(aula);
+		return "redirect:/aulas";
 	}
 }
