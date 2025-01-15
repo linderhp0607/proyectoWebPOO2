d6895199b4c9b55473df345359a8e3ab615640d7
andyganoza <andyganozasalas@gmail.com>
2025-01-15 18:19:04 -0500

    commit 8, crud completo de alumnos

--- codigo/proyectoWebPoo2/src/main/java/com/app/proyecto/alumno/alumnoControlador.java
+++ codigo/proyectoWebPoo2/src/main/java/com/app/proyecto/alumno/alumnoControlador.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.app.proyecto.aula.aula;
@@ -41,4 +42,22 @@ public class alumnoControlador {
 		return "redirect:/alumnos";
 	}
 	
+	@GetMapping("/alumnos/editar/{id}")
+	public String formularioEditar(@PathVariable("id") Integer id, Model modelo) {
+		Alumno alumno=repositorio.findById(id).get();
+		modelo.addAttribute("alumno", alumno);
+		List<aula> listaAulas=aulaRepository.findAll();
+		modelo.addAttribute("listaAulas", listaAulas);
+		return "alumnoFormulario";
+	}
+	
+	@GetMapping("/alumnos/eliminar/{id}")
+	public String eliminarAlumno(@PathVariable("id") Integer id) {
+	    if (repositorio.existsById(id)) {
+	        repositorio.deleteById(id);
+	    } else {
+	        throw new IllegalArgumentException("El alumno con ID " + id + " no existe.");
+	    }
+	    return "redirect:/alumnos";
+	}
 }
